4968b5694c67d99d18184e712be67a6cd028bc86
unknown <Administrador@UNI-LABF-7378.fuvs.br>
2026-05-07 21:09:56 -0300

    feat:estrutura inicial da pokedex

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Desenvolvido por [Mikael Augusto Lima Lopes] — Programação Frontend — UNIVAS</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+  return (
+    <div>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/563.png"
+        alt="Pikachu"
+      />
+      <h2>#0563 — Cofagrigus</h2>
+      <p>Tipo: Fantasma</p>
+    </div>
+  )
+}
+
+export default PokemonCard
